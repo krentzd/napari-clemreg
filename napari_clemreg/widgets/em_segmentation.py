20a885ce55d570ba3ed2ee1c3a2667c2369b3833
Daniel Krentzel <dkrentzel@klee.imod.pasteur.fr>
2021-12-15 15:23:12 +0100

    Adding DL feats

--- napari_clemreg/widgets/em_segmentation.py
+++ napari_clemreg/widgets/em_segmentation.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python3
 # coding: utf-8
 from magicgui import magic_factory
-from napari.layers import Labels, Image
-import numpy as np
+# from napari.layers import Labels, Image
+# import numpy as np
 
-# from sparse_unet.dataloader import SparseDataGenerator
+# from dataloader import SparseDataGenerator
 # from sparse_unet.model import SparseUnet
+
+from skimage import transform
+from skimage import exposure
+from skimage.exposure import equalize_hist
+
+from keras.layers import Conv2D
+from keras.layers import BatchNormalization
+from keras.layers import ReLU
+from keras.layers import MaxPooling2D
+from keras.layers import Conv2DTranspose
+from keras.layers import Input
+from keras.layers import Concatenate
+from keras.layers import Reshape
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import CSVLogger
+from keras.callbacks import Callback
+from keras.models import Model
+
+from .sparse_unet.dataloader import SparseDataGenerator
+from .sparse_unet.utils import SampleImageCallback
+from .sparse_unet.utils import weighted_categorical_crossentropy
+from .sparse_unet.utils import dice_coefficient
 
 import os
 import random
@@ -26,7 +48,7 @@ from keras import backend as K
 from keras.utils import Sequence
 
 from napari.layers import Labels, Image
-
+import math
 # TODO: Implement DataGenerator that uses label file as training data and automatically crops it
 
 class SparseDataGenerator(Sequence):
@@ -78,14 +100,11 @@ class SparseDataGenerator(Sequence):
                               3))
 
         curr_idx = self.layer_idx[idx % len(self.layer_idx)]
-        print(curr_idx)
 
         for batch_idx in range(self.batch_size):
-            print(batch_idx)
             img_crop, mask_crop = self._random_crop(self.source[curr_idx],
                                                     self.target[curr_idx],
                                                     self.crop_shape)
-            print(img_crop.shape, mask_crop.shape)
         # # Apply data augmentation
         # # Affine
         # # Gamma augmentation
@@ -113,16 +132,15 @@ class SparseDataGenerator(Sequence):
         w_0 = n_1 / (n_0 + n_1)
         w_1 = n_0 / (n_0 + n_1)
 
-        return (w_0, w_1, 0)
+        # return (w_0, w_1, 0)
+        return (1, 3, 0)
 
     def _random_crop(self, img, mask, crop_shape):
-        print(img.shape, mask.shape)
         width, height = crop_shape
         assert img.shape[0] >= height
         assert img.shape[1] >= width
         assert img.shape[0] == mask.shape[0]
         assert img.shape[1] == mask.shape[1]
-        print('Random crop')
         x = random.randint(0, img.shape[1] - width)
         y = random.randint(0, img.shape[0] - height)
 
@@ -134,14 +152,173 @@ class SparseDataGenerator(Sequence):
     def _z_score(self, input):
         return (input - np.mean(input)) / np.std(input)
 
+class SparseUnet:
+    def __init__(self, shape=(256,256,1)):
+
+        self.shape = shape
+
+        input_img = Input(self.shape, name='img')
+
+        self.model = self.unet_2D(input_img)
+
+    def down_block_2D(self, input_tensor, filters):
+
+        x = Conv2D(filters=filters, kernel_size=(3,3), padding='same')(input_tensor)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+
+        x = Conv2D(filters=filters*2, kernel_size=(3,3), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+
+        return x
+
+    def up_block_2D(self, input_tensor, concat_layer, filters):
+
+        x = Conv2DTranspose(filters, kernel_size=(2,2), strides=(2,2))(input_tensor)
+
+        x = Concatenate()([x, concat_layer])
+
+        x = Conv2D(filters=filters, kernel_size=(3,3), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+
+        x = Conv2D(filters=filters*2, kernel_size=(3,3), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+
+        return x
+
+    def unet_2D(self, input_tensor, filters=32):
+
+        d1 = self.down_block_2D(input_tensor, filters=filters)
+        p1 = MaxPooling2D(pool_size=(2,2), strides=(2,2))(d1)
+        d2 = self.down_block_2D(p1, filters=filters*2)
+        p2 = MaxPooling2D(pool_size=(2,2), strides=(2,2))(d2)
+        d3 = self.down_block_2D(p2, filters=filters*4)
+        p3 = MaxPooling2D(pool_size=(2,2), strides=(2,2))(d3)
+
+        d4 = self.down_block_2D(p3, filters=filters*8)
+
+        u1 = self.up_block_2D(d4, d3, filters=filters*4)
+        u2 = self.up_block_2D(u1, d2, filters=filters*2)
+        u3 = self.up_block_2D(u2, d1, filters=filters)
+
+        # Returns one-hot-encoded semantic segmentation mask where 0 is bakcground, 1 is mito and 2 is None (weight zero)
+        output_tensor = Conv2D(filters=3, kernel_size=(1,1), activation='softmax')(u3)
+
+        return Model(inputs=[input_tensor], outputs=[output_tensor])
+
+    def train(self,
+              src,
+              tgt,
+              out_dir,
+              epochs=100,
+              batch_size=32,
+              dense=False,
+              log_name='log.csv',
+              model_name='sparse_unet'):
+
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        if not os.path.exists(os.path.join(out_dir, 'ckpt')):
+            os.makedirs(os.path.join(out_dir, 'ckpt'))
+
+        train_generator = SparseDataGenerator(src,
+                                              tgt,
+                                              batch_size=batch_size,
+                                              crop_shape=self.shape[:2])
+
+        val_generator = SparseDataGenerator(src,
+                                            tgt,
+                                            batch_size=batch_size,
+                                            crop_shape=self.shape[:2],
+                                            augment=False)
+
+        sample_batch = val_generator[0][0]
+        sample_img = SampleImageCallback(self.model,
+                                         sample_batch,
+                                         out_dir,
+                                         save=True)
+
+        weight_zero, weight_one, weight_two = train_generator.batch_weights()
+
+        self.model.compile(optimizer='adam',
+                           loss=weighted_categorical_crossentropy(np.array([weight_zero, weight_one, weight_two])),
+                           metrics=[dice_coefficient])
+
+        self.model.summary()
+
+        # self.model.load_weights(model_name)
+
+        csv_logger = CSVLogger(os.path.join(out_dir, log_name))
+
+        ckpt_name =  'ckpt/' + model_name + '_epoch_{epoch:02d}_val_loss_{val_loss:.4f}.hdf5'
+
+        model_ckpt = ModelCheckpoint(os.path.join(out_dir, ckpt_name),
+                                     verbose=1,
+                                     save_best_only=False,
+                                     save_weights_only=True)
+
+        self.model.fit_generator(generator=train_generator,
+                                 validation_data=val_generator,
+                                 validation_steps=math.floor(len(val_generator))/batch_size,
+                                 epochs=epochs,
+                                 shuffle=False,
+                                 callbacks=[csv_logger,
+                                            model_ckpt,
+                                            sample_img])
+
+    def predict(self, input, tile_shape):
+
+        pad_in = np.zeros((math.ceil(input.shape[0] / tile_shape[0]) * tile_shape[0], math.ceil(input.shape[1] / tile_shape[1]) * tile_shape[1]))
+
+        pad_in[:input.shape[0],:input.shape[1]] = input
+
+        pad_out = np.zeros(pad_in.shape)
+
+        for x in range(math.ceil(input.shape[0] / tile_shape[0])):
+            for y in range(math.ceil(input.shape[1] / tile_shape[1])):
+                x_a = x * tile_shape[0]
+                x_b = x * tile_shape[0] + tile_shape[0]
+
+                y_a = y * tile_shape[1]
+                y_b = y * tile_shape[1] + tile_shape[1]
+                pad_out[x_a:x_b,y_a:y_b] = self.tile_predict(pad_in[x_a:x_b,y_a:y_b])
+
+        return pad_out[:input.shape[0],:input.shape[1]]
+
+    def tile_predict(self, input):
+
+        exp_input = np.zeros((1, self.shape[0], self.shape[1], 1))
+
+        exp_input[0,:,:,0] = exposure.equalize_hist(transform.resize(input, (self.shape[0], self.shape[1]), mode='constant', preserve_range=True))
+
+        return transform.resize(np.squeeze(self.model.predict(exp_input))[:,:,1], input.shape, mode='constant', preserve_range=True)
+
+    def save(self, model_name):
+        self.model.save_weights(model_name)
+
+    def load(self, model_name):
+        self.model.load_weights(model_name)
+
 
 @magic_factory
 def train_model(source: Image,
-                target: Labels) -> Image:
+                target: Labels): # -> Image:
 
-    train_generator = SparseDataGenerator(source, target, crop_shape=(512,512))
-    print('Len', len(train_generator))
+    # train_generator = SparseDataGenerator(source, target, crop_shape=(512,512))
+    # print('Len', len(train_generator))
 
-    cropped_images = train_generator[0][0]
+    model = SparseUnet(shape=(512,512,1))
 
-    return Image(cropped_images)
+    model.train(source,
+                target,
+                out_dir='output_dir',
+                epochs=10,
+                batch_size=16)
+
+    model.predict(source.data, tile_shape=(512,512))
+
+    # return Image(cropped_images)
--- napari_clemreg/widgets/sparse_unet/dataloader.py
+++ napari_clemreg/widgets/sparse_unet/dataloader.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 # coding: utf-8
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 
 import os
 import random
@@ -13,7 +9,7 @@ import imageio
 
 import numpy as np
 import tensorflow as tf
-import imgaug.augmenters as iaa
+# import imgaug.augmenters as iaa
 
 from skimage import transform
 from skimage import exposure
@@ -47,59 +43,61 @@ class SparseDataGenerator(Sequence):
 
         self.layer_idx = [i for (i, l) in enumerate(self.target) if np.sum(l) > 0]
 
-        self.seq = iaa.Sequential([
-                   iaa.Fliplr(0.5), # horizontal flips
-                   iaa.Flipud(0.5), # vertical flips
-                   iaa.Crop(percent=(0, 0.1)), # random crops
-                   iaa.Sometimes(0.5, iaa.GaussianBlur(sigma=(0, 0.5))),
-                   iaa.LinearContrast((0.75, 1.5)),
-                   iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255)),
-                   iaa.Multiply((0.8, 1.2)),
-                   iaa.Affine(
-                        scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
-                        translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-                        rotate=(-25, 25),
-                        shear=(-8, 8))
-                   ], random_order=True) # apply augmenters in random order
+        # self.seq = iaa.Sequential([
+        #            iaa.Fliplr(0.5), # horizontal flips
+        #            iaa.Flipud(0.5), # vertical flips
+        #            iaa.Crop(percent=(0, 0.1)), # random crops
+        #            iaa.Sometimes(0.5, iaa.GaussianBlur(sigma=(0, 0.5))),
+        #            iaa.LinearContrast((0.75, 1.5)),
+        #            iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255)),
+        #            iaa.Multiply((0.8, 1.2)),
+        #            iaa.Affine(
+        #                 scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+        #                 translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+        #                 rotate=(-25, 25),
+        #                 shear=(-8, 8))
+        #            ], random_order=True) # apply augmenters in random order
 
         # Initialise class
         # Identify layers with annotations
 
     def __getitem__(self, idx):
         img_batch = np.zeros((self.batch_size,
-                              crop_shape[0],
-                              crop_shape[1],
+                              self.crop_shape[0],
+                              self.crop_shape[1],
                               1))
         mask_batch = np.zeros((self.batch_size,
-                              crop_shape[0],
-                              crop_shape[1],
+                              self.crop_shape[0],
+                              self.crop_shape[1],
                               3))
 
         curr_idx = self.layer_idx[idx % len(self.layer_idx)]
+        print(curr_idx)
 
         for batch_idx in range(self.batch_size):
+            print(batch_idx)
             img_crop, mask_crop = self._random_crop(self.source[curr_idx],
-                                                    self.target[curr_idx])
-
-            # Apply data augmentation
-            # Affine
-            # Gamma augmentation
-
-
-            img_batch[batch_idx,...,0] = np.clip(self._z_score(img_crop), -1, 1)
-            mask_batch[batch_idx,...,0] = mask_crop == 1
-            mask_batch[batch_idx,...,1] = mask_crop == 2
-            mask_batch[batch_idx,...,2] = mask_crop == 0
-
-        return img_batch, mask_batch
+                                                    self.target[curr_idx],
+                                                    self.crop_shape)
+            print(img_crop.shape, mask_crop.shape)
+        # # Apply data augmentation
+        # # Affine
+        # # Gamma augmentation
+        #
+            img_batch[batch_idx,:,:,0] = np.clip(self._z_score(img_crop), -1, 1)
+            mask_batch[batch_idx,:,:,0] = mask_crop == 1
+            mask_batch[batch_idx,:,:,1] = mask_crop == 2
+            mask_batch[batch_idx,:,:,2] = mask_crop == 0
+        # print('got item')
+        return np.stack(img_batch), mask_batch
         # Augment data
         #Return batch
 
     def __len__(self):
-        __, x_full_ y_full = self.target.shape
-        x_crop, y_crop = self.crop_shape
-
-        return len(self.layer_idx) * (x_full / x_crop) * (y_full / y_crop)
+        # __, x_full_ y_full = self.target.shape
+        # x_crop, y_crop = self.crop_shape
+        return 1
+        # return len(self.layer_idx) * (x_full / x_crop) * (y_full / y_crop)
         # Length of datagenerator is (1 / aug_freq) * num_of_annotated_layers * (x_full / x_crop) * (y_full / y_crop)
 
     def batch_weights(self):
@@ -112,201 +110,20 @@ class SparseDataGenerator(Sequence):
         return (w_0, w_1, 0)
 
     def _random_crop(self, img, mask, crop_shape):
+        print(img.shape, mask.shape)
         width, height = crop_shape
         assert img.shape[0] >= height
         assert img.shape[1] >= width
         assert img.shape[0] == mask.shape[0]
         assert img.shape[1] == mask.shape[1]
-
+        print('Random crop')
         x = random.randint(0, img.shape[1] - width)
         y = random.randint(0, img.shape[0] - height)
 
-        img = img[y:y+height, x:x+width]
-        mask = mask[y:y+height, x:x+width]
+        img_crop = img[y:y+height, x:x+width]
+        mask_crop = mask[y:y+height, x:x+width]
 
-        return img, mask
+        return img_crop, mask_crop
 
     def _z_score(self, input):
-        return (self - np.mean(self)) / np.std(self)
-
-# class SparseDataGenerator(Sequence):
-#
-#     def __init__(self,
-#                  data_dir,
-#                  batch_size=32,
-#                  shape=(256,256,1),
-#                  dense=False,
-#                  augment=True):
-#
-#         self.dense = dense
-#         self.augment = augment
-#
-#         # Training Directory
-#         img_list = glob.glob(os.path.join(data_dir, 'images/*.png'))
-#         img_list.sort()
-#
-#         # Mask directory contains three sub-directories each containing
-#         # binary mask corresponding to foreground, background and None
-#         if self.dense:
-#             msk_list = glob.glob(os.path.join(data_dir, 'masks/*.png'))
-#             msk_list.sort()
-#
-#             self.data_list = list(zip(img_list,
-#                                       msk_list))
-#         else:
-#             msk_bg_list = glob.glob(os.path.join(data_dir, 'masks/background/*.png'))
-#             msk_fg_list = glob.glob(os.path.join(data_dir, 'masks/foreground/*.png'))
-#
-#             msk_bg_list.sort()
-#             msk_fg_list.sort()
-#
-#             self.data_list = list(zip(img_list,
-#                                       msk_bg_list,
-#                                       msk_fg_list))
-#
-#         self.on_epoch_end()
-#         self.batch_size = batch_size
-#         self.shape = shape
-#
-#     def __len__(self):
-#         if self.augment:
-#             return 2 * len(self.data_list) // self.batch_size
-#         else:
-#             return len(self.data_list) // self.batch_size
-#
-#     def __getitem__(self, idx):
-#
-#         # Instantiate batches
-#         img_batch = np.zeros((self.batch_size,
-#                               self.shape[0],
-#                               self.shape[1],
-#                               self.shape[2]))
-#         msk_batch = np.zeros((self.batch_size,
-#                               self.shape[0],
-#                               self.shape[1],
-#                               3))
-#
-#         # If densely annotated, background layer is 1-foreground and None class is 1-bg-fg in all cases
-#         k = idx % (len(self.data_list) // self.batch_size)
-#         for i in range(k * self.batch_size , k * self.batch_size + self.batch_size):
-#
-#             j = i % self.batch_size
-#
-#             img_batch[j, :, :, 0] = self._imread(self.data_list[i][0],
-#                                                  (self.shape[0], self.shape[1]),
-#                                                  'float')
-#             if self.dense:
-#                 msk_batch[j, :, :, 1] = self._imread(self.data_list[i][1],
-#                                                      (self.shape[0], self.shape[1]),
-#                                                      'float',
-#                                                      binary=True)
-#                 msk_batch[j, :, :, 0] = np.ones((self.shape[0], self.shape[1])) - msk_batch[j, :, :, 1]
-#             else:
-#                 msk_batch[j, :, :, 0] = self._imread(self.data_list[i][1],
-#                                                      (self.shape[0], self.shape[1]),
-#                                                      'float',
-#                                                      binary=True)
-#                 msk_batch[j, :, :, 1] = self._imread(self.data_list[i][2],
-#                                                      (self.shape[0], self.shape[1]),
-#                                                      'float',
-#                                                      binary=True)
-#
-#             msk_batch[j, :, :, 2] = np.ones((self.shape[0], self.shape[1])) - msk_batch[j, :, :, 1] - msk_batch[j, :, :, 0]
-#
-#         if self.augment:
-#
-#             # Roughly augment 50% of batches
-#             rand = np.random.random()
-#
-#             if rand > 0.5:
-#                 return img_batch, msk_batch
-#
-#             else:
-#                 seq = iaa.Sequential([
-#                     iaa.Fliplr(0.5), # horizontal flips
-#                     iaa.Flipud(0.5), # vertical flips
-#                     iaa.Crop(percent=(0, 0.1)), # random crops
-#                     iaa.Sometimes(
-#                         0.5,
-#                         iaa.GaussianBlur(sigma=(0, 0.5))
-#                     ),
-#                     iaa.LinearContrast((0.75, 1.5)),
-#                     iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255)),
-#                     iaa.Multiply((0.8, 1.2)),
-#                     iaa.Affine(
-#                         scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
-#                         translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-#                         rotate=(-25, 25),
-#                         shear=(-8, 8)
-#                     )
-#                 ], random_order=True) # apply augmenters in random order
-#
-#                 # For augmentation purposes, collapse segmentation masks from 4D to 3D
-#                 msk_batch_temp = np.zeros((msk_batch.shape[0],
-#                                            msk_batch.shape[1],
-#                                            msk_batch.shape[2]), dtype=np.int32)
-#                 msk_batch_temp[np.where(msk_batch[:,:,:,0] == 1)] = 0
-#                 msk_batch_temp[np.where(msk_batch[:,:,:,1] == 1)] = 1
-#                 msk_batch_temp[np.where(msk_batch[:,:,:,2] == 1)] = 2
-#                 msk_batch_temp = np.expand_dims(msk_batch_temp, axis=3)
-#
-#                 # msk_batch_temp = SegmentationMapsOnImage(msk_batch_temp, shape=(msk_batch.shape[1], msk_batch.shape[2], 1))
-#                 # Returns a list of images --> Must repopulate an array
-#                 img_aug_list, msk_aug_list = seq(images=(img_batch*255).astype(np.uint8),
-#                                                  segmentation_maps=msk_batch_temp)
-#
-#                 # Repopulate img array
-#                 img_batch_aug = np.zeros(img_batch.shape)
-#                 for i in range(len(img_aug_list)):
-#                     img_batch_aug[i] = img_aug_list[i]
-#
-#                 # Re-expand from collapsed mask to one-hot-encoded mask
-#                 msk_batch_aug = np.zeros((msk_batch.shape[0],
-#                                           msk_batch.shape[1],
-#                                           msk_batch.shape[2],
-#                                           3))
-#
-#                 # Assign entire collapsed mask to one channel and then set all but desired value to zero
-#                 for i, msk in enumerate(msk_aug_list):
-#                     msk = np.squeeze(msk)
-#                     msk_temp_zero = np.zeros(msk.shape)
-#                     msk_temp_zero[np.where(msk == 0)] = 1
-#                     msk_temp_one = np.zeros(msk.shape)
-#                     msk_temp_one[np.where(msk == 1)] = 1
-#                     msk_temp_two = np.zeros(msk.shape)
-#                     msk_temp_two[np.where(msk == 2)] = 1
-#                     msk_batch_aug[i,:,:,0] = msk_temp_zero
-#                     msk_batch_aug[i,:,:,1] = msk_temp_one
-#                     msk_batch_aug[i,:,:,2] = msk_temp_two
-#
-#                 img_batch_aug = (img_batch_aug/255).astype(np.float64)
-#
-#                 return img_batch_aug, msk_batch_aug
-#
-#         else:
-#             return img_batch, msk_batch
-#
-#     def on_epoch_end(self):
-#
-#         random.shuffle(self.data_list)
-#
-#     def batch_weights(self):
-#         # Compute from sample of 100 masks --> must ensure that None class is not counted...
-#         # Weight of class zero is all zero masks added divided by all
-#         return (1, 3, 0)
-#
-#     def _imread(self, img_path, size, dtype, binary=False):
-#
-#         try:
-#             img = imageio.imread(img_path, pilmode='L').astype(np.float)
-#
-#             resized_img = transform.resize(img, size, mode='constant', preserve_range=True)
-#             out_img = exposure.rescale_intensity(resized_img, out_range=dtype)
-#
-#             if binary:
-#                 return np.squeeze(out_img) > 0
-#             else:
-#                 return color.rgb2gray(out_img)
-#         except ValueError:
-#             print('Corrupted filename: ', img_path)
-#             return np.zeros(size)
+        return (input - np.mean(input)) / np.std(input)
