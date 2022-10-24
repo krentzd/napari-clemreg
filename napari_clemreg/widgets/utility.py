4741ec3a8a83f266855bf0666d35ff82c8f6d06f
MatousE <matous.elphick@gmail.com>
2022-10-24 13:33:06 +0100

    UPDATE - utility.py - Adding rough documentation and formatting

--- napari_clemreg/widgets/utility.py
+++ napari_clemreg/widgets/utility.py
@@ -9,6 +9,7 @@ import math
 from skimage import draw
 from typing_extensions import Annotated
 
+
 def on_init(widget):
     """Initializes widget layout and updates widget layout according to user input."""
 
@@ -30,11 +31,13 @@ def on_init(widget):
     widget.input.changed.connect(change_z_max)
     widget.z_min.changed.connect(change_z_max_from_z_min)
 
+
 @magic_factory(widget_init=on_init, layout='vertical', call_button="Mask")
 def mask_roi(input: Image,
              crop_mask: Shapes,
-             z_min: Annotated[int, {"min": 0, "max": 10, "step": 1}]=0,
-             z_max: Annotated[int, {"min": 10, "max": 100, "step": 1}]=10) -> Image: #Annotated[slice, {"start": 0, "stop": 10, "step": 1}]
+             z_min: Annotated[int, {"min": 0, "max": 10, "step": 1}] = 0,
+             z_max: Annotated[int, {"min": 10, "max": 100,
+                                    "step": 1}] = 10) -> Image:  # Annotated[slice, {"start": 0, "stop": 10, "step": 1}]
     """
     Take crop_mask and mask input
     """
@@ -47,23 +50,24 @@ def mask_roi(input: Image,
     print(input.data.shape)
 
     if crop_mask.data[0].shape[-1] > 3:
-        crop_mask.data = [mask[:,-3:] for mask in crop_mask.data]
+        crop_mask.data = [mask[:, -3:] for mask in crop_mask.data]
 
     assert len(crop_mask.data) == 1, 'Crop mask must contain one shape'
 
-    top_idx = crop_mask.shape_type.index('polygon') if 'polygon' in crop_mask.shape_type else crop_mask.shape_type.index('rectangle')
+    top_idx = crop_mask.shape_type.index(
+        'polygon') if 'polygon' in crop_mask.shape_type else crop_mask.shape_type.index('rectangle')
     top_z = z_min
 
     bot_z = z_max
 
-    input_arr = input.data #np.squeeze(input.data)
+    input_arr = input.data  # np.squeeze(input.data)
     print(input_arr.shape)
 
     temp_idx = 2 if len(input_arr.shape) == 4 else 1
 
     print(input_arr.shape[temp_idx:])
 
-    binary_mask = draw.polygon2mask(input_arr.shape[temp_idx:], crop_mask.data[top_idx][:,1:])
+    binary_mask = draw.polygon2mask(input_arr.shape[temp_idx:], crop_mask.data[top_idx][:, 1:])
     top_vol = [np.zeros(input_arr.shape[temp_idx:])] * top_z
     binary_mask_vol = [binary_mask] * (bot_z - top_z)
     bot_vol = [np.zeros(input_arr.shape[temp_idx:])] * (input_arr.shape[temp_idx - 1] - bot_z)
