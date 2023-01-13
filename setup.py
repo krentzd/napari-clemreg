00a2d7f981081c82868e837e36937abf2266dc22
MatousE <matous.elphick@gmail.com>
2023-01-13 10:38:34 +0000

    Fix - Commenting out requirements and setup.py as they are not required for npe2

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from setuptools import setup
-
-setup()
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+# from setuptools import setup
+#
+# setup()
